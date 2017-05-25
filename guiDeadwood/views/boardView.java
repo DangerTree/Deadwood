ff16bbc3b6f05df661c9ef17dac54d5c3f73a5be
Your Name <you@example.com>
2017-05-25 14:43:50 -0700

    added resources and views dirs

---
+++ guiDeadwood/views/boardView.java
@@ -0,0 +1,23 @@
+// boardView organizes roompanels?
+
+import javax.swing.JLayeredPane;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
+
+public class BoardView extends JLayeredPane{
+
+  public BoardView
+
+
+
+
+
+
+
+
+
+
+
+
+}
