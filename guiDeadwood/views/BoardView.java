d4894a4f03bf2ae56514def974109ca9b4a4b486
DangerTree <s.gunderson.00@gmail.com>
2017-05-26 17:23:43 -0700

    started SceneView.java. working on Resources.java class.

--- guiDeadwood/views/BoardView.java
+++ guiDeadwood/views/BoardView.java
@@ -11,8 +11,10 @@ public class BoardView extends JLayeredPane{
 
   public BoardView (model.Board bModel) throws Exception {
     ImageIcon icon = new ImageIcon (ImageIO.read(cls.getResourceAsStream("../resources/fullBoard.jpg"))); // board image
-
-
+    boardLabel.setIcon (icon);
+    add(boardLabel, new Integer (0)); // add the board image as the first layer
+    boardLabel.setBounds (0, 0, screenSize.width, screenSize.height);
+    setBounds (boardLabel.getBounds());
 
 
 
