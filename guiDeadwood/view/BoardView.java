9c6080112d8582823b0f674d29729c579eb2d097
DangerTree <s.gunderson.00@gmail.com>
2017-05-30 13:32:33 -0700

    a throwaway commit to sync with Zak

--- guiDeadwood/view/BoardView.java
+++ guiDeadwood/view/BoardView.java
@@ -31,8 +31,7 @@ public class BoardView extends JLayeredPane{
     boardLabel = new JLabel (backgroundImg);
     boardLabel.setBounds (0, 0, backgroundImg.getIconWidth(), backgroundImg.getIconHeight());
     add(boardLabel, new Integer (0)); // add the board image as the first layer
-    setBounds (boardLabel.getBounds());
-
+    this.setBounds (boardLabel.getBounds());
 
     // create room view objects
     makeR_SViews(bModel);
